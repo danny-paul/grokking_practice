0001cfb25623ecc3e621407ccc75a1366faefd22
danny <70734435+danny-paul@users.noreply.github.com>
2023-04-25 14:30:39 -0400

    initial commit

---
+++ binary_search.py
@@ -0,0 +1,4 @@
+# returns index position of value if found, else reutrns -1
+def binary_search(arr: list[int]) -> int:
+    print("bin_search")
+    return -1
---
+++ main.py
@@ -0,0 +1,10 @@
+import binary_search as search_lib
+
+def main():
+    ordered_list = {1, 2, 5, 6, 44, 55, 88, 99}
+    
+    print("test, in main")
+    search_lib.binary_search(ordered_list)
+    
+if __name__ == "__main__":
+    main()
